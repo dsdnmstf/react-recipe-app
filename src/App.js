28568a03c9f483b190246e12e7a0839bfd2ab246
musrafa <dsdnmstf@gmail.com>
2022-05-30 00:18:34 +0200

    routers added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-  return <div>APP</div>;
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
