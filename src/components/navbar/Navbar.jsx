92152cd3b434e40124c5abab20101e7ffa381866
musrafa <dsdnmstf@gmail.com>
2022-06-06 21:33:49 +0200

    .

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <Nav justify="space-between" wrap="wrap">
       <Logo to="/">
-        <i>{"<Mustafa's>"}</i>
+        <i>{"<MY>"}</i>
         <span>RECIPES</span>
       </Logo>
 
